7df8c7d300fbfef26fd5214ebfd11db2e2ead2b5
WeavileFan <elliotreichstein2@gmail.com>
2025-10-22 19:41:12 -0700

    Create mode.js

---
+++ mode.js
@@ -0,0 +1,12 @@
+const mode = document.getElementById("mode");
+const text = document.getElementById("text");
+
+mode.addEventListener("click", () => {
+  if (document.body.style.backgroundColor == "black") {
+    text.style.Color = white";
+  mode.textContent = "Dark Mode";
+} else {
+      document.body.style.backgroundColor == "white")
+    text.style.Color = "black";
+  mode.textContent = "Light Mode";
+  
